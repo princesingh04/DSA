d8daa25eb9a1b6f90dfe10611fd3c523e66ddfce
Prince Singh <princesinghss786@gmail.com>
2025-08-01 23:19:56 +0530

    1500 - Sweets-Eating

---
+++ Codeforces/TLE/1400/1500/sweets-eating.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to repeat a string 'times' times
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+  long long n,m;
+  cin>>n>>m;
+  vector<long long> v(n);
+  for(long long i=0; i<n; i++) cin>>v[i];
+    sort(v.begin(),v.end());
+vector<long long> pre(n,0);
+for(long long i=0; i<n; i++){
+    if(i>0){ pre[i] = v[i]+pre[i-1];}
+    else 
+    {
+        pre[i] = v[i];
+    }
+    }
+   vector<long long> ans(n,0);
+   
+   for(long long i=0; i<n; i++){
+   if(i>=m) ans[i] = pre[i]+ans[i-m];
+   else ans[i] = pre[i];
+   }
+   for(auto it:ans) cout<<it<<" ";
+    cout<<endl;
+
+    return 0;
+
+}
+//2 5 11 18 30 43 62 83 121
